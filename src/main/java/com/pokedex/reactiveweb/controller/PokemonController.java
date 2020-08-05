1a7e1e6daaaa6ac2fa5892c74128a8030a148c3e
Will <willianwed98@gmail.com>
2020-08-05 14:21:51 -0300

    Event Class and Event method (Showing a event stream)

--- src/main/java/com/pokedex/reactiveweb/controller/PokemonController.java
+++ src/main/java/com/pokedex/reactiveweb/controller/PokemonController.java
@@ -1,5 +1,9 @@
 package com.pokedex.reactiveweb.controller;
 
+import java.time.Duration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pokedex.reactiveweb.model.Pokemon;
+import com.pokedex.reactiveweb.model.PokemonEvent;
 import com.pokedex.reactiveweb.repository.PokemonRepository;
 
 import reactor.core.publisher.Flux;
@@ -39,6 +45,7 @@ public class PokemonController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Mono<Pokemon> insertPokemon(@RequestBody Pokemon obj){
 		return repository.save(obj);
 	}
@@ -70,5 +77,12 @@ public class PokemonController {
 	@DeleteMapping
 	public Mono<Void> deleteAllPokemons(){
 		return repository.deleteAll();
+	}
+	
+	@GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<PokemonEvent> getPokemonEvents(){
+		return Flux.interval(Duration.ofSeconds(5))
+				.map(val ->
+				new PokemonEvent(val, "Product Event"));
 	}
 }
